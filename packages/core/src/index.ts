10374ca7dc333ddaeeee4a44961d7a4664153121
tiger-oshima <oshima.automate.growth@gmail.com>
2026-09-01 19:25:15 +0900

    公開APIを整理

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,19 @@
-export * from "./AccessableDataStore";
-export * from "./query";
-export * from "./SheetDB";
-export * from "./SheetGateway";
-export * from "./SheetRelation";
-export * from "./SheetTable";
+export { SheetDB } from "./SheetDB";
+export type {
+  CurrentRecord,
+  CurrentTable,
+  NestedCreateInput,
+  NestedCreateResult,
+} from "./SheetDB";
+export { SheetGateway } from "./SheetGateway";
+export { SheetTable } from "./SheetTable";
+export type { AutoNumberingMode, Columns } from "./SheetTable";
+export { SheetQuery } from "./query/SheetQuery";
+export type {
+  JoinedRecord,
+  Relationable,
+  SheetJoin,
+  TableByName,
+} from "./query/SheetQuery";
+export { SheetRelation } from "./SheetRelation";
+export type { OnDeleteAction } from "./SheetRelation";

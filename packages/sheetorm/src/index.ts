4d863530511d07e66168ee0bb581a382616f1737
大島大河 <oshima.automate.growth@gmail.com>
2026-09-12 15:46:22 +0900

    SheetsStubを公開

--- packages/sheetorm/src/index.ts
+++ packages/sheetorm/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./core/SheetDB";
 export * from "./core/SheetTable";
 export * from "./gateway/SheetGateway";
+export * from "./storage/SheetsStub";
 export * from "./storage/SpreadsheetAppStub";
